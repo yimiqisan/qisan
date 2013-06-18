18f1906089b52d835adc0993599a26dd7bf647f6
yimiqisan <yimiqisan@gmail.com>
2013-06-18 16:37:27 +0800

    add conf

--- apps/words/runserver.py
+++ apps/words/runserver.py
@@ -7,7 +7,7 @@ Created by 刘 智勇 on 2013-06-16.
 Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 """
 
+from words import app
 
 if __name__ == '__main__':
-    from words import app
-#    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
